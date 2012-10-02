d5e51ffa47e23f994f3adc525af210226daa625b
cesine <cesine@yahoo.com>
2012-10-02 13:24:30 -0400

    Revert "Removed sample Activity stuff and made it into a bunch of static functions instead."
    This reverts commit 38930bddea295e234e9cce597db8380ebed01ee9.

--- src/com/google/code/p/leveldb/LevelDB.java
+++ src/com/google/code/p/leveldb/LevelDB.java
@@ -15,7 +15,133 @@
  */
 package com.google.code.p.leveldb;
 
-public class LevelDB {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class LevelDB extends Activity {
+	String mDBdir;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		/*
+		 * Use the files dir to store the database /data/data/package..../db
+		 */
+		mDBdir = this.getFilesDir().getAbsolutePath() + File.separator + "db";
+		new File(mDBdir).mkdirs();
+
+	}
+
+	@Override
+	protected void onResume() {
+		/*
+		 * Sample Database code: Open the database using the path to its
+		 * directory Insert some keys, Delete a key, Create a TextView and show
+		 * the value of a key retrieved from the DB.
+		 */
+		dbDestroy(mDBdir);
+		dbOpen(mDBdir);
+		dbPut("firstkey", "this is the value of the first key");
+		dbPut("secondkey", "this is the value of the first key");
+		dbPut("keyToDelete",
+				"this is the value of the key that i want to delete");
+		dbPut("fourthkey", "this is the value of the fourth key");
+		dbDelete("keyToDelete");
+
+		TextView tv = new TextView(this);
+		tv.setText(dbGet("fourthkey"));
+		setContentView(tv);
+
+		ArrayList<String> keystoquery = new ArrayList<String>();
+		try {
+			AssetManager assetManager = getAssets();
+			InputStream in = assetManager.open("sample.json");
+			BufferedReader sourcefile = new BufferedReader(
+					new InputStreamReader(in, "UTF-8"));
+			String contents = "";
+			String line = "";
+			while ((line = sourcefile.readLine()) != null) {
+				contents = contents + "\n" + line;
+			}
+			sourcefile.close();
+			JSONObject json = new JSONObject(contents);
+			Iterator<String> keys = json.keys();
+			while (keys.hasNext()) {
+				String key = (String) keys.next();
+				keystoquery.add(key);
+				dbPut(key, json.get(key).toString());
+			}
+
+			int j = json.length() - 1;
+			tv.setText("element" + j);
+			setContentView(tv);
+
+		} catch (IOException e) {
+			Toast.makeText(this, "File read problem" + e.getLocalizedMessage(),
+					Toast.LENGTH_LONG).show();
+			tv.setText("File read problem" + e.getLocalizedMessage());
+			setContentView(tv);
+		} catch (JSONException e) {
+			Toast.makeText(this, "Json problem" + e.getLocalizedMessage(),
+					Toast.LENGTH_LONG).show();
+			tv.setText("JSON problem" + e.getLocalizedMessage());
+			setContentView(tv);
+		}
+
+		/*
+		 * Query entries randomly
+		 */
+		long startime = System.currentTimeMillis();
+		int maxkey = keystoquery.size() - 1;
+		int querycount = 1000;
+		Random randomGenerator = new Random();
+		for (int k = 0; k < querycount; k++) {
+			int randomKey = randomGenerator.nextInt(maxkey);
+			String it = dbGet(keystoquery.get(randomKey));
+		}
+		long endtime = System.currentTimeMillis();
+		long querytime = (endtime - startime);
+		tv.setText("Random quering of " + querycount
+				+ " entries took this many miliseconds: " + querytime);
+		setContentView(tv);
+
+		super.onResume();
+	}
+
+	private String readInFile(String filePath) throws IOException {
+		File currentFile = new File(filePath);
+		BufferedReader sourcefile = new BufferedReader(new InputStreamReader(
+				new FileInputStream(currentFile), "UTF-8"));
+		String contents = "";
+		String line = "";
+
+		while ((line = sourcefile.readLine()) != null) {
+
+			contents = contents + "\n" + line;
+
+		}
+		sourcefile.close();
+		return contents;
+	}
+
 	/*
 	 * Methods which wrap LevelDB calls, see jni/main.cc for details
 	 */
@@ -57,5 +183,14 @@ public class LevelDB {
 	 */
 	static {
 		System.loadLibrary("leveldb");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		/*
+		 * Close the db in the onPause to not waste memory
+		 */
+		dbClose(mDBdir);
 	}
 }
